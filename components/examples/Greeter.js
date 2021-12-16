65d3d079be5d9f524a75b3ab24ea629ccf6b4559
Christof <bauer.christof@gmail.com>
2021-12-15 22:16:33 -0500

    Update Greeter.js

--- components/examples/Greeter.js
+++ components/examples/Greeter.js
@@ -1,8 +1,10 @@
 import GreeterContract from '../../src/artifacts/contracts/Greeter.sol/Greeter.json'
 import { useState, useRef } from 'react'
 import { ethers } from 'ethers'
+import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader'
 
 export const Greeter = () => {
+  const [receiving, setReceiving] = useState(false)
   const [greeting, setGreetingState] = useState('')
   const [newGreeting, setNewGreetingState] = useState('')
   const [greetingMessage, setGreetingMessageState] = useState('')
@@ -10,12 +12,14 @@ export const Greeter = () => {
 
   // Call smart contract, fetch current value
   async function fetchGreeting() {
+    setReceiving(true)
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const contract = new ethers.Contract(process.env.NEXT_PUBLIC_GREETER_ADDRESS, GreeterContract.abi, provider)
     try {
       const data = await contract.greet()
       setGreetingState(data)
       setGreetingMessageState(`Blockchain response: ${data}`)
+      setReceiving(false)
     } catch (error) {
       console.log(error)
     }
@@ -27,7 +31,7 @@ export const Greeter = () => {
       setGreetingMessageState('Add a new greeting first.')
       return
     }
-    // await requestAccount()
+    setReceiving(true)
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const signer = provider.getSigner()
     const contract = new ethers.Contract(process.env.NEXT_PUBLIC_GREETER_ADDRESS, GreeterContract.abi, signer)
@@ -39,12 +43,13 @@ export const Greeter = () => {
     newGreetingInputRef.current.value = ''
     setGreetingState('')
     setNewGreetingState('')
+    setReceiving(false)
   }
 
   return (
     <div className="flex flex-col gap-6 w-full items-center">
       <div className="flex gap-4">
-        <button className="button" onClick={fetchGreeting}>
+        <button className="button" onClick={fetchGreeting} disabled={receiving}>
           Fetch greeting from the blockchain
         </button>
         <input type='text' disabled
@@ -55,21 +60,31 @@ export const Greeter = () => {
       </div>
 
       <div className="flex gap-4">
-        <input type='text'
+        <input type='text' disabled={receiving}
           className="border p-4 text-center"
           onChange={e => setNewGreetingState(e.target.value)}
           placeholder="Write a new greeting"
           ref={newGreetingInputRef}
         />
-        <button className="button" onClick={setGreeting} >
+        <button className="button" onClick={setGreeting} disabled={receiving}>
           Set new greeting on the blockchain
         </button>
       </div>
 
-      <div className="border border-dashed border-brand-dark dark:border-brand p-4">
-        {greetingMessage && <span>{greetingMessage}</span>}
-      </div>
-
+      {receiving ?
+        <>
+          <div className='flex dark:hidden mt-8'>
+            <ClimbingBoxLoader size={15} color='var(--color-brand-dark)' />
+          </div>
+          <div className='hidden dark:flex mt-8'>
+            <ClimbingBoxLoader size={15} color='var(--color-brand)' />
+          </div>
+        </>
+        :
+        <div className="border border-dashed border-brand-dark dark:border-brand p-4">
+          {greetingMessage && <span>{greetingMessage}</span>}
+        </div>
+      }
     </div>
   )
 }
