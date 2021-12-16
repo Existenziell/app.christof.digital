0d942bfb4ece1fafdbeb4bd154f6b62943897084
Christof <bauer.christof@gmail.com>
2021-12-16 01:04:07 -0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,13 +5,13 @@ const Footer = () => {
       bg-brand dark:bg-brand-dark dark:border-brand dark:border-opacity-10'>
 
       <a href='#top' aria-label='Scroll back up'>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 hover:text-brand transition-colors" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 hover:text-brand-dark dark:hover:text-brand transition-colors" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
         </svg>
       </a>
 
       <a href='https://www.christof.digital/coding' className='absolute left-0' aria-label='Back Home'>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-9 hover:text-brand transition-colors cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-9 hover:text-brand-dark dark:hover:text-brand transition-colors cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
         </svg>
       </a>
