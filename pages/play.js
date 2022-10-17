92ac2e3939507d485e0c79d9a45886a58b23a191
Christof <bauer.christof@gmail.com>
2022-10-17 01:38:45 -0500

    SVGs

--- pages/play.js
+++ pages/play.js
@@ -1,9 +1,24 @@
-import SVGs from '../components/SVGs'
+import { useState } from 'react'
+import Arrow from '../components/SVG/Arrow'
+import Grid from '../components/SVG/Grid'
+import Phone from '../components/SVG/Phone'
 
 const Play = () => {
+  const [drawing, setDrawing] = useState(false)
   return (
-    <div>
-      <SVGs />
+    <div className='flex flex-col items-center justify-center'>
+      {!drawing ?
+        <div className='flex flex-col items-center justify-center'>
+          <Arrow setDrawing={setDrawing} />
+          <Phone />
+        </div>
+        :
+        <div className='max-h-[calc(50vh)]'>
+          <Grid setDrawing={setDrawing} />
+        </div>
+      }
+      <div className='h-32'>
+      </div>
     </div>
   )
 }
